7caf2cc8e73d73517b46b822102d54abda7a3e6f
João Victor Martins Rey <jvreysoad762@gmail.com>
2021-04-29 15:24:05 -0400

    Initial Commit

---
+++ src/main/java/vvt/CalculadoraComJunit/CalculadoraComJunitApplication.java
@@ -0,0 +1,13 @@
+package vvt.CalculadoraComJunit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculadoraComJunitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculadoraComJunitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vvt/CalculadoraComJunit/CalculadoraComJunitApplicationTests.java
@@ -0,0 +1,13 @@
+package vvt.CalculadoraComJunit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculadoraComJunitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
