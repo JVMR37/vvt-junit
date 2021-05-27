341bba459d36c1b4ca8efd53198586afb37952fd
João Victor Martins Rey <jvreysoad762@gmail.com>
2021-05-27 15:44:18 -0400

    Ajustes na loja e adição testes da loja

--- src/Loja.java
+++ src/Loja.java
@@ -1,31 +1,60 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Loja {
-	public static int funcCont = 0;
+    public static int funcCont = 0;
     public String nome;
     public String[] funcionarios;
+    public List<String> produtos;
 
     public Loja(String nome) {
         this.nome = nome;
+        produtos = new ArrayList<String>();
+        iniciarConexaoComBanco();
     }
 
     public boolean cadastraProduto(String nomeProduto) {
-        if (nomeProduto.startsWith("A")) {
+        if (Character.isUpperCase(nomeProduto.charAt(0))) {
+            produtos.add(nomeProduto);
             return true;
-        }
-
-        else {
+        } else {
             return false;
         }
     }
-    
+
     public boolean addFuncionario(String funcionario, int idade) {
-    	if (idade >= 18) {
-    		funcionarios[funcCont] = funcionario;
-    		funcCont++;
-    		return true;
-    	}
-    	
-    	else {
-    		return false;
-    	}
+        if (idade >= 18) {
+            funcionarios[funcCont] = funcionario;
+            funcCont++;
+            return true;
+        } else {
+            return false;
+        }
     }
+
+    public String comunicarComApi1() throws InterruptedException {
+        Thread.sleep(100);
+
+        return "Retorno da comunicação com a API";
+    }
+
+    public String comunicarComApi2() throws InterruptedException {
+        Thread.sleep(10);
+
+        return "Retorno da comunicação com a API";
+    }
+
+    public boolean verificaProduto(String nomeProduto) {
+        return produtos.contains(nomeProduto);
+    }
+
+    private void  iniciarConexaoComBanco(){
+        System.out.println("CONEXÃO COM O BANCO EFETUADA COM SUCESSO !");
+    }
+
+    public void fecharConexaoComBD() {
+        System.out.println("CONEXÃO COM O BANCO ENCERRADA COM SUCESSO !");
+    }
+
+
 }
--- src/LojaTest.java
+++ src/LojaTest.java
@@ -1,9 +1,22 @@
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
 
+import java.time.Duration;
+
+@DisplayName("Testes da Classe Loja")
 public class LojaTest {
 
-    private final Loja loja = new Loja("Loja de produtos que começam apenas com A");
+    private static Loja loja;
+
+
+    @BeforeAll
+    static void iniciaConexaoComBanco() {
+        System.out.println("=========================================");
+
+        loja = new Loja("Loja de produtos que começam apenas com letras Maiúsculas");
+
+        System.out.println("=========================================");
+    }
 
 
     //teste de nulidade de um objeto
@@ -30,4 +43,81 @@ public class LojaTest {
         String funcionario = "Pedro";
         Assertions.assertFalse(loja.addFuncionario(funcionario, 16));
     }
+
+    // O método teste se a chamada retornará com 10ms
+    @Test
+    void testeComunicacaoComAPIComTimeoutExcedente() {
+        Assertions.assertTimeout(Duration.ofMillis(10), () -> {
+
+            String retorno = loja.comunicarComApi1();
+
+            System.out.println(retorno);
+        });
+    }
+
+    @Test
+    void testeComunicacaoComAPIComTimeoutSemExcedente() {
+        Assertions.assertTimeout(Duration.ofMillis(20), () -> {
+
+            String retorno = loja.comunicarComApi2();
+
+            System.out.println(retorno);
+        });
+    }
+
+    @Test
+    void testeDeObjetosIguais() {
+        Loja loja = new Loja("Loja 1");
+        Loja mesmaLoja = loja;
+        Assertions.assertSame(loja, mesmaLoja);
+    }
+
+    @Test
+    void testesDeObjetosDiferentes() {
+        Loja loja = new Loja("Loja 1");
+        Loja loja2 = new Loja("Loja 2");
+        Assertions.assertSame(loja, loja2);
+    }
+
+    @Test
+    @DisplayName("TESTE DE ADIÇÃO DE PRODUTOS")
+    void testeAdicaoDeProdutos() {
+        // Dentro de um bloco de código, se uma declaração falhar,
+        // o código subsequente no mesmo bloco será ignorado.
+        Assertions.assertAll("Adição de produtos",
+                () -> {
+                    loja.cadastraProduto("Abacaxi");
+                    loja.cadastraProduto("Abacate");
+                    loja.cadastraProduto("Amaciante");
+
+                    // Em uma asserção agrupada, todas as asserções são executadas
+                    // e todas as falhas serão relatadas juntas.
+                    Assertions.assertAll("Verficação de cadastro dos produtos",
+                            () -> Assertions.assertTrue(loja.verificaProduto("Abacaxi")),
+                            () -> Assertions.assertTrue(loja.verificaProduto("Abacate")),
+                            () -> Assertions.assertTrue(loja.verificaProduto("Amaciante"))
+                    );
+
+                    Assertions.assertFalse(loja.produtos.contains("Banana"));
+                }
+        );
+
+    }
+
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @AfterAll
+    static void tearDownAll() {
+
+        System.out.println("=========================================");
+
+        loja.fecharConexaoComBD();
+
+        System.out.println("=========================================");
+    }
+
+
 }
